b1224a18de990ca6e04b5b2d0ce02a9edf68406e
Christopher Wheatley <robwheat@hotmail.com>
2018-11-10 23:00:22 -0800

    Initial Commit

---
+++ MyAppIOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAppIOS
+//
+//  Created by Christopher Wheatley on 11/10/18.
+//  Copyright © 2018 Christopher Wheatley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
